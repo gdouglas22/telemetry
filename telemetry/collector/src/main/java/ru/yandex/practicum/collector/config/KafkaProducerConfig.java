eb2ea2fbec68decbc15c44076844fbff109cad6b
Bogdan <bogdan@macbook-air-bogdan.home>
2026-07-03 20:43:49 +0200

    Serialize Kafka messages with plain Avro binary encoding

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaProducerConfig.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.collector.config;
 
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -11,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,16 +20,12 @@ public class KafkaProducerConfig {
     @Value("${collector.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Value("${collector.kafka.schema-registry-url}")
-    private String schemaRegistryUrl;
-
     @Bean
     public ProducerFactory<String, SpecificRecordBase> producerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
